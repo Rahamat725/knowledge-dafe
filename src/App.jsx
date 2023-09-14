1c7ff6d1aae00748a70781dbe8c5756a4793ccea
Sk Rahamatullah <skrahamatullah500@gmail.com>
2023-09-14 19:15:44 +0530

    setup proptype

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import Header from './components/Header/Header'
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
+import Bookmarks from './components/Bookmarks/Bookmarks';
 
 function App() {
-const [blogs, setBlogs] = useState([]);
 
-useEffect(()=>{
-  fetch('blogs.json')
-  .then(res=>res.json())
-  .then(data=>setBlogs(data))
-},[])
 
   return (
     <>
-     <h2>Data: {blogs.length}</h2>
+     {/* <h2>Data: {blogs.length}</h2> */}
       <Header></Header>
-      <Blogs></Blogs>
+     <div className='md:flex justify-between'>
+     <Blogs></Blogs>
+     <Bookmarks></Bookmarks>
+     </div>
       
     </>
   )
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,9 +1,17 @@
-
+import { useEffect, useState } from 'react'
 
 const Blogs = () => {
+    const [blogs, setBlogs] = useState([]);
+
+useEffect(()=>{
+  fetch('blogs.json')
+  .then(res=>res.json())
+  .then(data=>setBlogs(data))
+},[])
+
     return (
-        <div>
-            <h2>Blogs: </h2>
+        <div className="md:w-2/3">
+            <h2 className="text-3xl">Blogs: {blogs.length}</h2>
         </div>
     );
 };
